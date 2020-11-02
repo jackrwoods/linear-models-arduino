5564b4acea9e07539d24264fbc8f7819fb5b9030
Jack Woods <jackrwoods@gmail.com>
2020-11-01 16:16:22 -0800

    Finish library

--- LinearModelEvaluator.cpp
+++ LinearModelEvaluator.cpp
@@ -1,43 +1,60 @@
 #include "LinearModelEvaluator.h"
 
-LinearModel::LinearModel() {
-	this->availableSize = 3; // Start with three
-	this->length = 0;
-	this->variables = new ExplanatoryVariable[this->availableSize];
+#define NULL 0
+
+ExplanatoryVariable::ExplanatoryVariable(double coefficient, double (*evaluate)(double)) {
+	this->coefficient = coefficient;
+	this->evaluate = evaluate;
+	this->link = NULL;
 }
 
-LinearModel::addVariable(double coefficient, double (*evaluate)(double)) {
-	// Is there enough space?
-	if (length >= availableSize) {
-		ExplanatoryVariable* temp = this->variables;
-		this-> availableSize++;
-		variables = new ExplanatoryVariable[this->availableSize];
-		memcpy(variables, temp, sizeof(ExplanatoryVariable) * (this->availableSize - 1));
-		delete[] temp;
+LinearModel::LinearModel() {
+	this->variables = NULL;
+	this->tail = NULL;
+}
+
+void LinearModel::addVariable(double coefficient, double (*evaluate)(double)) {
+	if (!this->variables) {
+		this->variables = new ExplanatoryVariable(coefficient, evaluate);
+		this->tail = this->variables;
+	} else {
+		this->tail->link = new ExplanatoryVariable(coefficient, evaluate);
+		this->tail = this->tail->link;
+	}
+}
+
+int LinearModel::removeVariable(int index) {
+	ExplanatoryVariable* current = this->variables;
+	ExplanatoryVariable* previous;
+	while (index > 0 && current->link) {
+		index--;
+		previous = current;
+		current = current->link;
 	}
 
-	// There is enough space now!
-	// Add the new explanatory variable to the array
-	this->variables[this->length] = ExplanatoryVariable(coefficient, evaluate);
-	this->length++;
+	// We found the element!
+	if (index == 0) {
+		// Remove it from the linked list
+		previous->link = current->link;
+		delete current;
+		return 0;
+	} else {
+		return 1; // Exception handling is disabled on arduino?
+	}
+
 }
 
-LinearModel::removeVariable(int index) {
-	memcpy(&this->variables[index], &this->variables[index + 1], sizeof(ExplanatoryVariable) * (this->length - (index + 1)));
-	this->length--;
-}
-
-LinearModel::evaluate(double* input) {
+double LinearModel::evaluate(double* input) {
 	double result = 0;
-	for (int i = 0; i < this->length; i++) {
+	int i = 0;
+	ExplanatoryVariable* current = this->variables;
+	while (current) {
 		// Evaluate each explanatory variable and add its result to "result"
-		result += this->variables[i].coefficient * this->variables[i].evaluate(input[i]);
+		result += current->coefficient * current->evaluate(input[i]);
+		i++;
+		current = current->link;
 	}
 
 	return result;
 }
 
-ExplanatoryVariable::ExplanatoryVariable(double coefficient, double (*evaluate)(double)) {
-	this->coefficient = coefficient;
-	this->evaluate = evaluate;
-}
